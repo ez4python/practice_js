58cebb170212212819410b568d529833371f05f4
ez4python <akbaralisalohiddinov808@gmail.com>
2024-07-30 18:51:06 +0500

    some updates

---
+++ data_types.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// Data Types
+
+// Number
+let number = 5.6;
+
+// Infinity
+// console.log(-8 / 0);
+
+// NaN
+// console.log('Salom' * 2);
+
+// String
+let clientName = 'Akbarali'
+let clientSurname = `Salohiddinov`
+let clientBirthplace = "Uzbekistan"
+
+// Boolean
+let isMarried = false;
+
+// Null
+// console.log(clientAge)
+
+// Undefined
+let und;
+
+// Object
+const thief = {
+    // properties
+    // key -> value
+    age: 30,
+    jacket: 'black',
+    isLong: false
+}
+
+// Array
+const arr = ['haha', 20, true, [], {}]
