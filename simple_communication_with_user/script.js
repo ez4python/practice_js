64fdc2253ab66de5023ed6da8f31328c557673fa
ez4python <akbaralisalohiddinov808@gmail.com>
2024-07-31 10:25:22 +0500

    Today I learned about simple communication with user

---
+++ simple_communication_with_user/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// let user_age = prompt('Let me know your age:')
+// console.log(user_age)
+
+// const place = confirm('Are you here ?')
+// console.log(place)
+
+// const currentPlace = prompt('Where are you know ?', 'Uzbekistan')
+// console.log(currentPlace) // type: string
+
+// const age = +prompt('How old are you ?')
+// console.log(age + 1) // output: 13, 'cause of '+' in front of "prompt"
+// console.log(typeof age) // output: number
+
+const favouriteAnimals = []
+
+favouriteAnimals[0] = prompt('What is your favourite animal #1', '')
+favouriteAnimals[1] = prompt('What is your favourite animal #2', '')
+favouriteAnimals[2] = prompt('What is your favourite animal #3', '')
+
+console.log(favouriteAnimals)
+console.log(typeof favouriteAnimals)
