8d90d7711a5acc6b504899e979709511f53cd5e1
ez4python <akbaralisalohiddinov808@gmail.com>
2024-07-31 15:22:14 +0500

    deleted 'web' directry, and added some new information, added '.idea' directory to .gitignore

---
+++ interpolation/script.js
@@ -0,0 +1,7 @@
+let channel = prompt('Enter you favourite youtube channel', 'apple')
+
+// console.log('https://youtube.com/' + channel + '/videos')
+// console.log("https://youtube.com/" + channel + "/videos")
+// console.log(`https://youtube.com/${channel}/videos`)
+
+
---
+++ operators/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let incr = 10
+let decr = 10
+
+// incr++
+// decr--
+
+// console.log(10 % 3)
+// console.log(5 * 5 == 25) // output: true, 'causer the meaning is the same regardless of the type
+// console.log(6 * 6 === '36') // output: false, 'cause of type
+
+// console.log(9 - '3') // output: 6
+// console.log(10 + '12') // output: 1012
+// console.log(10 + +'12') // output: 22
+
+// && -> 'and'
+// || -> 'or'
+// const isAged = true;
+// const isClosed = true;
+// console.log(isAge && isClose) // output: true
+
+
