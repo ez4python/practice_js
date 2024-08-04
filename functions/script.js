5dc66f9edf56f947cb1ca029f25fa006b3bd6978
ez4python <akbaralisalohiddinov808@gmail.com>
2024-08-04 11:58:52 +0500

    Add: functions lesson

---
+++ functions/script.js
@@ -0,0 +1,22 @@
+'use strict';
+// Function Declaration
+// function calc(a, b) {
+//     return a + b;
+// }
+//
+// const sumOfTwo = calc(10, 5);
+// console.log(sumOfTwo);
+
+// Function Expression
+// const logger = function () {
+//     console.log('Hello World!');
+// };
+//
+// logger();
+
+// Arrow Function
+// const calc = (a, b) => a + b;
+// const foo = (a, b, c) => {
+//     console.log("O'rta qiymat!");
+//     return a + b + c / 2;
+// }
