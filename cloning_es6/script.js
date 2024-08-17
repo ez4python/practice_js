753edcdb4d9129c9ae89bf08bc9c97f191efc1ad
ez4python <akbaralisalohiddinov808@gmail.com>
2024-08-17 19:14:53 +0500

    Add: cloning & ES6

---
+++ cloning_es6/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// let x = 10;
+// let y = x;
+//
+// y = y + 5;
+//
+// console.log(x);
+// console.log(y);
+//
+// let isMarried = false;
+// let isArray = isMarried;
+//
+// isArray = true;
+//
+// console.log(isArray);
+// console.log(isMarried);
+
+// const numbers = {
+//     x: 10,
+//     y: 5,
+// }
+//
+// const newNumbers = numbers;
+//
+// newNumbers.x = 15;
+//
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const number = {
+//     x: 10,
+//     y: 5,
+//     z: {
+//         a: 1,
+//         b: 2,
+//     },
+// }
+//
+// const nums = {
+//     a: 7,
+//     b: 8
+// }
+//
+// const newNumber = Object.assign(nums, number);
+// newNumber.x = 15;
+//
+// console.log(number);
+// console.log(newNumber);
+
+// const arr = [1, 2, 3];
+// const assigned_arr = Object.assign([], arr);
+// const sliced_arr = arr.slice();
+//
+// sliced_arr[1] = 'white';
+// console.log(arr);
+// console.log(sliced_arr);
+
+// SPREAD operator
+// const group_321 = ['Alisher', 'Sultan', 'Akbarali'];
+// const group_320 = ["Ulug'bek", "Og'abek", 'Iskandar'];
+// const patok = [...group_321, ...group_320];
+// console.log(patok);
