bd3043399e68c45e75fedf0fbcb241d3ff57da7b
ez4python <akbaralisalohiddinov808@gmail.com>
2024-08-17 20:14:13 +0500

    Add: spread operator

---
+++ spread/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const calc = [1, 2, 3]
+
+function logger(a, b, c) {
+    console.log(a + b + c);
+}
+
+// logger(1, 2, 3);
+// logger(...calc);
+
+
+const numbers = {
+    x: 10,
+    y: 5
+}
+
+const newNumbers = {...numbers}
+// console.log(numbers);
+// console.log(newNumbers);
+
+
