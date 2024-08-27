aad58c3e22dac890ad49182b200fc89a54420f0a
ez4python <akbaralisalohiddinov808@gmail.com>
2024-08-27 18:18:57 +0500

    [LESSON] "tab" project

---
+++ 2-module/classlist/js/delegation.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const wrapper = document.querySelector('.btn-block'),
+  btns = document.querySelectorAll('button')
+
+btns[0].addEventListener('click', () => {
+  btns[1].classList.toggle('red')
+})
+
+wrapper.addEventListener('click', (event) => {
+  // event.target.tagName === 'BUTTON'
+  // event.target.classList.contains('red')
+  if (event.target && event.target.matches('button.red')) {
+    console.log('btn click')
+  }
+})
+
+// btns.forEach(item => {
+//   item.addEventListener('click', () => {
+//     console.log('clicked')
+//   })
+// })
+
+const btn = document.createElement('button')
+btn.classList.add('red')
+wrapper.append(btn)
---
+++ 2-module/pets/pet-1/js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const tabParent = document.querySelector('.tabheader__items'),
+    tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent')
+
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      // item.style.display = 'none'
+      item.classList.add('hide')
+      item.classList.remove('show', 'fade')
+    })
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active')
+    })
+  }
+
+  function showTabContent(idx = 0) {
+    // tabsContent[idx].style.display = 'block'
+    tabsContent[idx].classList.add('show', 'fade')
+    tabsContent[idx].classList.remove('hide')
+    tabs[idx].classList.add('tabheader__item_active')
+  }
+
+  hideTabContent()
+  showTabContent()
+
+  tabParent.addEventListener('click', (event) => {
+    const target = event.target
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, idx) => {
+        if (target === item) {
+          hideTabContent()
+          showTabContent(idx)
+        }
+      })
+    }
+  })
+})
